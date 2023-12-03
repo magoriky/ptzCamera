3698ce6126dea02a7a8826102e3a33d7ebf03428
Ricardo Ortiz <ricardoortizpozo@gmail.com>
2023-12-03 17:26:21 +0900

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ricardo/Desktop/ptzCamera/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ricardo/Desktop/alfredo/devel;/home/ricardo/Desktop/hex2/devel;/home/ricardo/Desktop/sam/devel;/home/ricardo/handsfree/handsfree_ros_ws/devel;/home/ricardo/Desktop/imu_driver/devel;/home/ricardo/Desktop/code_for_github/rover_tesis_github/devel;/opt/ros/melodic'.split(';') if '/home/ricardo/Desktop/alfredo/devel;/home/ricardo/Desktop/hex2/devel;/home/ricardo/Desktop/sam/devel;/home/ricardo/handsfree/handsfree_ros_ws/devel;/home/ricardo/Desktop/imu_driver/devel;/home/ricardo/Desktop/code_for_github/rover_tesis_github/devel;/opt/ros/melodic' != '' else []
---
+++ build/description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ricardo/Desktop/ptzCamera/src/description
+# Build directory: /home/ricardo/Desktop/ptzCamera/build/description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
