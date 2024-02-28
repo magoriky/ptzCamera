0b1e03801c680300dad12cecdc5c68ace51d4a7b
Ricardo Ortiz <ricardoortizpozo@gmail.com>
2024-02-28 10:53:50 +0900

    backup

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("cv_bridge_tutorial_pkg")
 subdirs("slidingmcontroller")
 subdirs("description")
 subdirs("main")
---
+++ build/cv_bridge_tutorial_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ricardo/Desktop/ptzCamera/src/cv_bridge_tutorial_pkg
+# Build directory: /home/ricardo/Desktop/ptzCamera/build/cv_bridge_tutorial_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
